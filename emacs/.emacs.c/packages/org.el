bda56f98848b1ce60aa78e5f03a2794b3aa89507
Lyx <yojan.bustamante36@gmail.com>
2023-12-03 21:21:57 -0500

    rewritten the org mode configurations

--- emacs/.emacs.c/packages/org.el
+++ emacs/.emacs.c/packages/org.el
@@ -1,37 +1,31 @@
 ;; org configurations are here
+(require 'org)
 (require 'org-bullets)
+
+;; Use org-bullets
+(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+
+;; Hide delimiters
+(setq org-hide-emphasis-markers t)
+
+;; Customize bullet symbols
+(setq org-bullets-bullet-list '("☀" "✿" "☽" "■"))
+
 (add-hook 'org-mode-hook
 					(lambda ()
-						(dolist (face '((org-level-1 . 1.2)
-														(org-level-2 . 1.1)
-														(org-level-3 . 1.05)
-														(org-level-4 . 1.0)
-														(org-level-5 . 1.1)
-														(org-level-6 . 1.1)
-														(org-level-7 . 1.1)
-														(org-level-8 . 1.1)))
-							(set-face-attribute (car face) nil :font "Cantarell" :weight 'regular :height (cdr face)))
-
-						(set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
-						(set-face-attribute 'org-code nil		:inherit '(shadow fixed-pitch))
-						(set-face-attribute 'org-table nil	 :inherit '(shadow fixed-pitch))
-						(set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
-						(set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
-						(set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
-						(set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch)
-
-						(font-lock-add-keywords 'org-mode
-																		'(("^ *\\([-]\\) "
-																			 (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
-
-						(setq org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")
-									org-ellipsis " ▾"
-									visual-fill-column-width 100
-									visual-fill-column-center-mode t
-									org-hide-emphasis-markers t)
-
 						(org-indent-mode)
-						(org-bullets-mode 1)
-						(visual-line-mode 1)
-						(variable-pitch-mode 1)
-						(visual-fill-column-mode 1)))
+						(setq org-adapt-indentation nil)
+						(setq-local line-spacing 0.2) ; Add spacing between lines if needed
+						))
+
+;; Customize faces for different heading levels and increase font size
+(custom-set-faces
+ '(org-level-1 ((t (:foreground "#268bd2" :weight bold :height 1.2))))
+ '(org-level-2 ((t (:foreground "#859900" :weight bold :height 1.1))))
+ '(org-level-3 ((t (:foreground "#b58900" :height 1.05))))
+ '(org-level-4 ((t (:foreground "#cb4b16"))))
+ '(org-level-5 ((t (:foreground "#6c71c4"))))
+ '(org-level-6 ((t (:foreground "#2aa198"))))
+ '(org-level-7 ((t (:foreground "#268bd2"))))
+ '(org-level-8 ((t (:foreground "#859900"))))
+ )
